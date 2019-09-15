d6f9acb705766c136fd5540180bcddf078013b73
USUARIO <USUARIO@PC9>
2019-09-15 00:46:29 -0500

    Se agregan mas opciones

--- src/proyectoprueba/Proyectoprueba.java
+++ src/proyectoprueba/Proyectoprueba.java
@@ -8,12 +8,25 @@ public class Proyectoprueba {
     public static void main(String[] args) {
         Scanner sc =  new Scanner (System.in); 
        int menu = 0;
-       
+       do {
+           System.out.println("Bienvenidos al horario"
+                   + "Digite el numero segun la materia"
+                   + "1. Matematicas"
+                   + "2. Sociales"
+                   + "3. Ingles"
+                   + "4. Salir");
        switch (menu){
            case 1:
+               System.out.println("el horario es de 7-8");
+           break;
+           case 2:
+               System.out.println("el horario es de 7-8");
+           break;
+           case 3:
+               System.out.println("el horario es de 7-8");
            break;
        
        }
-    }
-    
+    }while(menu>0 && menu <=3);
+   } 
 }
